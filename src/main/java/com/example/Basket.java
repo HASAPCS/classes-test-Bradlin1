7355bb2c321caa54c3220f0caf914070d3839f46
Bradlin1 <bradlin9999@gmail.com>
2023-10-06 13:33:01 +0800

    done

--- src/main/java/com/example/Basket.java
+++ src/main/java/com/example/Basket.java
@@ -8,15 +8,35 @@ public class Basket {
 
     public Basket(int capacity) {
         // TODO: Initialize the attributes (and initialize the fruits list)
+        this.capacity = capacity;
+        capacity=0;
+        
     }
 
     public void addFruit(Fruit fruit) {
         // TODO: Add the fruit to the basket IF THERE IS SPACE!
+        if (this.fruits.size()<this.capacity){
+            this.fruits.add(fruit);
+            
+        }
     }
 
     public void removeFruit(Fruit fruit) {
         // TODO: Remove the fruit from the basket
+        this.fruits.remove(fruit);
+        
+    }
+    // TODO: Implement getters, setters, and any other methods you find necessary
+    public ArrayList<Fruit> getFruitList() {
+        return fruits;
     }
 
-    // TODO: Implement getters, setters, and any other methods you find necessary
-}
+    public int getCapacity() {
+        return this.capacity;
+    }
+    public void setCapacity(int newCapacity) {
+        this.capacity = newCapacity;
+    }
+
+} 
+
